22b8628a1af366de2053b84515352b360a771c6d
liuhuacun <liuhuacun@hotmail.com>
2024-05-27 18:40:13 +0800

    modify vue

--- vueDemo/src/App.vue
+++ vueDemo/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <Test title="Why Vue is so fun" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
---
+++ vueDemo/src/components/Test.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+defineProps(['title'])
+const count = ref(0)
+</script>
+
+<template>
+      <h4>{{ title }}</h4>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
