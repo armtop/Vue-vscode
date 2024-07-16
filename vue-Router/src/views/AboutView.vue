e964b8396f244a914c091b2b570990589b6d3fb9
liuhuacun <liuhuacun@hotmail.com>
2024-07-16 21:47:14 +0800

    modify AboutView

--- vue-Router/src/views/AboutView.vue
+++ vue-Router/src/views/AboutView.vue
@@ -30,9 +30,6 @@
       <RouterView />
     </main>
   </div>
-  <router-view class="view one"></router-view>
-  <router-view class="view two" name="a"></router-view>
-  <router-view class="view three" name="b"></router-view>
 </template>
 
 <style>
