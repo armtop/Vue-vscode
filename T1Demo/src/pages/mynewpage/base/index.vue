c443ce55cbf212ca1dcfb82e4efd4f7287cae55e
armtop_tom <lhc_tom@126.com>
2024-08-16 14:24:34 +0800

    modify

--- T1Demo/src/pages/mynewpage/base/index.vue
+++ T1Demo/src/pages/mynewpage/base/index.vue
@@ -9,13 +9,28 @@ export default {
 const a = 1;
 </script>
 <template>
-  <div>
-    <p>{{ a }}</p>
-    <h1>{{ $t('pages.result.networkError.title') }}</h1>
-    <h1>{{ $t('pages.mydynamicpage.messages.greeting') }}</h1>
-    <h1>{{ $t('pages.result.messages.greeting') }}</h1>
-    <p>
-      <my-extended-button new-prop="custom value"> {{ $t('components.msg') }} </my-extended-button>
-    </p>
-  </div>
+  <t-space direction="vertical">
+    <div>
+      <t-layout>
+        <t-header>
+          <p>Header</p>
+          <p>{{ a }}</p>
+        </t-header>
+        <t-layout>
+          <t-content>
+            <p>Content</p>
+            <p>{{ $t('pages.result.networkError.title') }}</p>
+            <p>
+              <my-extended-button new-prop="custom value"> {{ $t('components.msg') }} </my-extended-button>
+            </p>
+          </t-content>
+          <t-aside>Aside {{ $t('pages.mydynamicpage.messages.greeting') }}</t-aside>
+        </t-layout>
+        <t-footer>
+          <p>Footer</p>
+          <p>{{ $t('pages.result.messages.greeting') }}</p>
+        </t-footer>
+      </t-layout>
+    </div>
+  </t-space>
 </template>
